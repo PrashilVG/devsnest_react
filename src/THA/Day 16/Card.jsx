de9d705daea8a7f0e13984b23025d84da93bf70b
Prashil Gajjar <prashilgajjar7@gmail.com>
2021-07-25 18:51:16 +0530

    First Commit

---
+++ src/THA/Day 16/Card.jsx
@@ -0,0 +1,11 @@
+  
+const Card = () => {
+    return (
+      <div className="card">
+        <img src="meme.jpg" alt="meme image"></img>
+        <h1>REACT CARD</h1>
+      </div>
+    );
+  };
+  
+  export default Card;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
+import Card from "./THA/Day 16/Card";
+import Card2 from "./THA/Day 16/Card2";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "../src/";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(<Card />, document.getElementById("root"));
+ReactDom.render(<Card2 />, document.getElementById("root"));
+
