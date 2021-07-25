3ab488b8c717251d08486a48a86dea22f0b48fe2
Prashil Gajjar <prashilgajjar7@gmail.com>
2021-07-26 00:08:14 +0530

    Day 20

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDom from "react-dom";
 import Card from "./THA/Day 16/Card";
 import Card2 from "./THA/Day 16/Card2";
+import Calorie_card from "./THA/Day 17/Calorie_card";
+import Board from "./THA/Day 18/Board";
+import App from "./THA/Day 19/App";
+import Calorie_card1 from "./THA/Day 18/CalorieList";
+import Calorie_card2 from "./THA/Day 20/Calorie_card2";
+import "./index.css";
 
-import "../src/";
-
-ReactDom.render(<Card />, document.getElementById("root"));
-ReactDom.render(<Card2 />, document.getElementById("root"));
-
+// ReactDom.render(<Card />, document.getElementById("root"));
+// ReactDom.render(<Card2 />, document.getElementById("root"));
+// ReactDom.render(<Board />, document.getElementById("root"));
+// ReactDom.render(<Calorie_card />, document.getElementById("root"));
+// ReactDom.render(<App />, document.getElementById("root"));
+// ReactDom.render(<Calorie_card1 />, document.getElementById("root"));
+ReactDom.render(<Calorie_card2 />, document.getElementById("root"));
